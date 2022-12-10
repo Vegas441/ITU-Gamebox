8b9cfb2b41a126e868cc3d9176619742d347c1f0
David <davo444ms@gmail.com>
2022-12-10 16:32:30 +0100

    Review posting system working, needs some pimpin up tho

--- src/RateCard.js
+++ src/RateCard.js
@@ -3,10 +3,70 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import CardHeader from 'react-bootstrap/esm/CardHeader';
 import ReactStars from 'react-stars';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 import { GrGamepad } from 'react-icons/gr';
+import GameCard from './GameCard';
+import axios from "axios";
 
 
 export default function RateCard({ game }) {
+
+  // Modal controls 
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  /**
+   * Gets the next id for new review from database
+   * @param {*} obj Database url
+   * @returns 
+   
+  const [obj, setObj] = useState({})  // Object hook for finding next index
+  async function getNextId() {
+    let i = 0
+    do {
+      i++;
+      await fetch(`http://localhost:8000/reviews/${i}`).then(res => res.json()).then(data => {setObj(data); console.log(data);})
+      console.log(obj)
+    } while(Object.keys(obj).length !== 0)
+    return i + 1
+  }
+  */
+
+  const postReview = reviewBody => {
+    try{
+      // Form an object
+      reviewBody.preventDefault()
+      const formData = new FormData(reviewBody.target),
+            formDataObj = Object.fromEntries(formData.entries())
+      const reviewBodyString = Object.values(formDataObj)[0]
+      //console.log(reviewBodyString)
+
+      setShow(false);
+      // Post data to database
+      /**
+       * TODO:
+       * rating system 
+       * user authentification
+       * new id 
+       */
+      const _id = 7
+      const _today = new Date();
+      axios.post('http://localhost:8000/reviews', {
+        id: _id, //getNextId(),
+        name: `review_${_id}`,
+        userID: 1,
+        gameID: game.id,
+        rating: 5,
+        content: reviewBodyString,
+        date: _today.toLocaleDateString()
+      })
+    } catch(error) {
+      alert(error)
+    }
+  }
+
   return (
     <>
       <Card
@@ -22,7 +82,9 @@ export default function RateCard({ game }) {
         </div>
         <hr/>
         <Card.Text>
-          <p style={{color: 'white', fontSize: '20px', marginLeft: '35%'}}>Review</p>
+          <Button variant='text' onClick={handleShow} style={{marginLeft: '30%'}}>
+          <p style={{color: 'white', fontSize: '20px'}}>Review</p>
+          </Button>
           <hr/>
           <p style={{color: 'white', fontSize: '20px', marginLeft: '30%'}}>Add to list</p>
           <hr/>
@@ -30,6 +92,33 @@ export default function RateCard({ game }) {
           <hr/>
         </Card.Text>
       </Card>
+
+      {/* Review modal */}
+      <Modal show={show} onHide={handleClose} >
+        <Modal.Header closeButton>
+          <Modal.Title>Write a review</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          {game && <GameCard game={game}/>}
+          <Form onSubmit={postReview}>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+              >
+              <Form.Label>Review body</Form.Label>
+              <Form.Control as="textarea" rows={6} name="myInput"/>
+            </Form.Group> 
+            
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" type="submit">
+              Post review
+            </Button>
+        
+          </Form>
+        </Modal.Body>
+        </Modal>
     </>
   )
 }
--- src/ReviewPage.js
+++ src/ReviewPage.js
@@ -39,19 +39,22 @@ export default function ReviewPage() {
                     alt=''
                     width="50"
                     height="50" 
-                    style={{borderRadius: '50%', marginTop: '-150px', marginLeft: '150px'}}
+                    style={{borderRadius: '50%', marginTop: '-150px', marginLeft: '75px'}}
                 />
                 }
                 <p style={{ color: 'white', fontSize: '22px',display: 'inline', position: 'relative', bottom: '70px', left: '15px' }}>Review by {user &&  <b>{user.name}</b>}</p>
+                <div style={{marginLeft: '-5%'}}>
                 <hr style={{color: '#14181c', width: '500px', marginLeft: '350px', marginTop: '-250px'}}/>
                 {game && <h1 style={{color: 'white', marginLeft: '350px', fontWeight: 'bold'}}>{game.name}
                 <p style={{ display: 'inline', fontSize: '20px', fontWeight: 'normal', marginLeft:'20px'}}>{game.releaseDate}</p>
-                <ReactStars size={32} count={review.rating} color1={'yellow'} edit={ false } style={{ display: 'inline'}}/></h1>}
+                <ReactStars size={32} count={review.rating} color1={'yellow'} edit={ false } style={{ display: 'inline'}}/></h1>
+                }
                 {game && <p style={{color: 'white', marginLeft: '350px', maxWidth: '700px'}}>{review.content}</p>}
+                </div>
 
                 {game && 
-                <div style={{float: 'right', marginTop: '-210px'}}>
-                    <RateCard/>
+                <div style={{float: 'right', marginTop: '-220px'}}>
+                    {game && <RateCard game={game}/>}
                 </div>}
             </div>
         }

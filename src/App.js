659bcec9eda0fb67a882e5ed254b2b08df8739ac
Vítězslav Cupl <vitezslavcupl@gmail.com>
2022-12-14 11:07:50 +0100

    games and search

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import ReviewsFromFriends from "./ReviewsFromFriends";
 import ReviewPage from "./ReviewPage";
 import GameboxNavbar from "./GameboxNavbar";
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import GamesPage from "./GamesPage";
+import GamePage from "./GamePage";
 
 
 function App() {
 
-  //const [games, setGames] = useState(null);
+  const [games, setGames] = useState(null);
     
   const [userProfiles, setProfiles] = useState(null);
     
@@ -28,6 +30,13 @@ function App() {
     .then(data => {
       setReviews(data);
     });
+    fetch('http://localhost:8000/games')
+    .then(res => {
+      return res.json();
+    })
+    .then(data => {
+      setGames(data);
+    })
   }, []);
 
 
@@ -37,6 +46,7 @@ function App() {
       { userProfiles && <GameboxNavbar 
       userProfile={userProfiles[0]}/>}
 
+
       <Routes>
 
         { reviews && <Route exact path="/" element={
@@ -45,6 +55,14 @@ function App() {
 
         <Route exact path="/reviewpage/:reviewID" element={
           <ReviewPage />
+        }/>
+
+        { games && <Route exact path="/gamespage" element={
+          <GamesPage games={games}/>
+        }/>}
+
+        <Route exact path="/gamespage/:gameID" element={
+          <GamePage />
         }/>
 
       </Routes>
--- src/GameboxNavbar.js
+++ src/GameboxNavbar.js
@@ -1,14 +1,32 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button'
 import { FaSearch } from 'react-icons/fa'
 import { Link } from 'react-router-dom';
+import SearchBar from './SearchBar'
+import Search from './Search.js'
 
 export default function GameboxNavbar({ userProfile }) {
-  return (
-    <>
+    const [games, setGames] = useState(null);
+    const [searchButton, setSearchButton] = useState(false);
+    const setSearchBtn = () => {
+      setSearchButton(true);
+    };
+
+    useEffect(() => {
+      fetch('http://localhost:8000/games')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setGames(data);
+      })
+    }, []);
+
+    return (
+    <>  
       <Navbar fixedTop style={{backgroundColor: "#14181c", variant: "light"}}>
         <Container>
           <Link to="/" style={{ textDecoration: 'none' }}>
@@ -34,13 +52,18 @@ export default function GameboxNavbar({ userProfile }) {
             />
             <a><Nav.Link style={{color: '#99aabb', size: '24px', fontWeight: 'bold'}}>{userProfile.name}</Nav.Link></a>
           
-            <a><Nav.Link style={{color: '#99aabb'}}> Games </Nav.Link></a>
+            <Link to="/gamespage" className='nav-link' style={{color: '#99aabb'}}> Games </Link>
             <a><Nav.Link style={{color: '#99aabb'}}> Lists </Nav.Link></a>
-            <a><Nav.Link><FaSearch style={{width: 30, color: '#99aabb'}}/></Nav.Link></a>
+            <button className="searchbutton" onClick={setSearchBtn}><FaSearch style={{width: 30, color: '#99aabb'}}/></button>
+            
+            
             <Button size='sm'> <b>+ LOG</b>  </Button>
           </Nav>
         </Container>
       </Navbar>
+        <SearchBar trigger={searchButton} setTrigger={setSearchButton}> 
+            <Search placeholder="Enter a Game Name" games={games} />
+        </SearchBar>
     </>
   )
 }
